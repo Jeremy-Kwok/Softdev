8cfbd227865a5b5c16538d6456a7bb9010ab40ec
Jeremy-Kwok <Jeremykwok917@gmail.com>
2022-09-21 19:12:09 -0400

    9/21

---
+++ 03_py/list2/sum67.py
@@ -0,0 +1,17 @@
+#Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the next 7 (every 6 will be followed by at least one 7). Return 0 for no numbers.
+
+def sum67(nums):
+    ignore = False
+    sum = 0
+    for x in nums:
+        if (x == 6):
+            ignore = True
+        if not ignore:
+            sum = sum + x
+        if (x == 7):
+            ignore = False
+    return sum
+
+print(sum67[1, 2, 2]) # 5
+print(sum67[1, 2, 2, 6, 99, 99, 7]) # 5
+print(sum67[1, 1, 6, 7, 2]) # 4
